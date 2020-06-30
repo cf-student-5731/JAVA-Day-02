cd9ecdeb80b8a6d87cd1f44de68436ef4aea6eaf
fyzam <fyzam@fasy.at>
2020-06-30 10:49:07 +0200

    BasicStrings method added

--- src/BasicStrings.java
+++ src/BasicStrings.java
@@ -1,11 +1,17 @@
 public class BasicStrings {
 
+
+    static char getChar(String s, int i){
+        return s.charAt(i);
+    }
+
     public static void main(String[] args) {
         String hannah = "Did Hannah see bees? Hannah did";
         System.out.println(hannah);
         System.out.println("This String has " + hannah.length() + " Characters inside!");
 
+            System.out.println(getChar(hannah, 12));
+
 
     }
-
 }
