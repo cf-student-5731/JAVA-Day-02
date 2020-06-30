0d95e07cb712dfeff8de65a94a9ebd47f07a535e
fyzam <fyzam@fasy.at>
2020-06-30 10:29:08 +0200

    BasicStrings added

---
+++ src/BasicStrings.java
@@ -0,0 +1,11 @@
+public class BasicStrings {
+
+    public static void main(String[] args) {
+        String hannah = "Did Hannah see bees? Hannah did";
+        System.out.println(hannah);
+        System.out.println("This String has " + hannah.length() + " Characters inside!");
+
+
+    }
+
+}
