196a3a4eb388c2cb28bbbdb1c576c0283b581fa0
fyzam <fyzam@fasy.at>
2020-06-30 11:21:27 +0200

    refer to character added

--- src/BasicStrings.java
+++ src/BasicStrings.java
@@ -5,12 +5,18 @@ public class BasicStrings {
         return s.charAt(i);
     }
 
+    static int getPositionOf(String s, char c){
+        return s.indexOf('b');
+    }
+
     public static void main(String[] args) {
         String hannah = "Did Hannah see bees? Hannah did";
         System.out.println(hannah);
         System.out.println("This String has " + hannah.length() + " Characters inside!");
 
             System.out.println(getChar(hannah, 12));
+            System.out.println(getPositionOf(hannah, 'b'));
+
 
 
     }
